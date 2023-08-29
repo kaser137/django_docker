c948fd5240d82b270581bb3058f4d14e8d2a8e08
kaser137 <murilon375@gmail.com>
2023-08-30 02:39:43 +0300

    edit

--- dj_doc/settings.py
+++ dj_doc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4gvz6g%93!%!_@g+8ugzb+9#j=er@u)u!5%++nq+zzzjm%xxaq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://147.182.233.67/']
 
 
 # Application definition
